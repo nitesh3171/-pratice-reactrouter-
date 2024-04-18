2ac851ff691391fb331d2010d7b33abeb2806eac
Nitesh Jadhav <niteshjadhav3171@gmail.com>
2024-04-18 10:04:15 +0530

    commit changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ function Home(){
     return(
         <div>
             <h1>This is the home page</h1>
-            <link To="about">Click to view about page</link>
-            <link To="contact">Click to view contact page</link>
+            <Link To="about">Click to view about page</Link>
+            <Link To="contact">Click to view contact page</Link>
 
         </div>
     );
