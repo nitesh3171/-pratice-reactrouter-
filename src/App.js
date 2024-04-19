1398dfe8a7174623b50b7044e479694d77088ff3
Nitesh Jadhav <niteshjadhav3171@gmail.com>
2024-04-19 13:41:07 +0530

    interface updated

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import {Routes ,Route} from 'react-router-dom';
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Navbars from './components/Navbars';
+import Interface from './components/Interface';
+
 function App() {
   return (
     <div >
+     <Navbars/>
+     <Interface/>
      <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="about" element={<About/>}/>
-      <Route path="contact" element={<Contact/>}/>
-
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
      </Routes>
     </div>
   );
