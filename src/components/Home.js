a0c40156cf5df048c2891507278fd55518241525
Nitesh Jadhav <niteshjadhav3171@gmail.com>
2024-04-19 13:41:35 +0530

    changes updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Link } from 'react-router-dom';
+// import {Link } from 'react-router-dom';
 function Home(){
     return(
         <div>
             <h1>This is the home page</h1>
-            <Link To="about">Click to view about page</Link>
-            <Link To="contact">Click to view contact page</Link>
+            {/* <Link to="/about">Click to view about page</Link> */}
+            {/* <Link to="/about">Click to view about page</Link>
+            <Link to="/contact">Click to view contact page</Link> */}
 
         </div>
     );
