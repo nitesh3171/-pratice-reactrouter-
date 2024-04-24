c6ec1e98d535d0978a6f3b35cf88556f986c4144
Nitesh Jadhav <niteshjadhav3171@gmail.com>
2024-04-24 17:47:53 +0530

    done some changes

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,16 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
-const CartPage = () => {
+const Cart = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  const removeFromCart = (index) => {
+    const newCartItems = [...cartItems];
+    newCartItems.splice(index, 1);
+    setCartItems(newCartItems);
+  };
+
+  const totalItems = cartItems.length;
+
   return (
     <div>
-      <h1>Cart Page</h1>
-      <Link to="/">
-        <button>Go Back</button>
-      </Link>
+      <h2>Shopping Cart</h2>
+      <p>Total Items: {totalItems}</p>
+      <ul>
+        {cartItems.map((item, index) => (
+          <li key={index}>
+            {item.name} - ${item.price}
+            <button onClick={() => removeFromCart(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => addToCart({ name: 'Product 1', price: 10 })}>
+        Add Product 1
+      </button>
+      <button onClick={() => addToCart({ name: 'Product 2', price: 20 })}>
+        Add Product 2
+      </button>
     </div>
   );
 };
 
-export default CartPage;
+export default Cart;
+
 
